56e67588bbf860cd94b13b32ec3f6761211ef325
Felix <felix@Megans-MBP-2.hitronhub.home>
2020-10-09 15:43:56 -0600

    Views and components added

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,147 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    <div>
+    <v-app-bar
+      
+      dense
+      dark
+    >
+    
+      <v-toolbar-title>Freephonics</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
+      
+
+    </v-app-bar>
+  </div>
+
+    <!-- Drawer -->
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      left
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- App Content - Router View -->
     <v-main>
-      <HelloWorld />
+      <router-view></router-view>
     </v-main>
+
+    <!-- Footer -->
+    <v-footer color="primary lighten-1" padless>
+    <v-layout justify-center wrap>
+      <v-btn
+        v-for="link in links"
+        :key="`${link.label}-footer-link`"
+        color="white"
+        text
+        rounded
+        class="my-2"
+        :to="link.url"
+      >
+        {{ link.label }}
+      </v-btn>
+      <v-flex primary lighten-2 py-4 text-center white--text xs12>
+        {{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
+      </v-flex>
+    </v-layout>
+  </v-footer>
+
   </v-app>
+    
+  
+
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+  export default {
+    name: "App",
+    data: () => ({
+      drawer: false,
+      group: null,
+      links: [
+        {
+          label: 'Home',
+          url: '/'
+        }, 
+        {
+          label: 'About', 
+          url: '/about'
+        }
+      ]
+    }),
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
 
-export default {
-  name: "App",
+    components: {
 
-  components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
-};
+    }
+  };
 </script>
+
+<style>
+  h1, h2, h3, h4, h5, h6{
+    font-family: 'Baloo Tammudu 2', cursive;
+  }
+
+  .v-toolbar__title{
+    font-family: 'Baloo Tammudu 2', cursive;
+    font-weight: 700;
+  }
+
+  p{
+    font-family: 'Lato', sans-serif;
+  }
+
+  .v-app-bar {
+    background: linear-gradient(-45deg, #ee7752, #ce4b7e, #23a6d5, #23d5ab);
+    background-size: 400% 400%;
+    animation: gradient 5s ease infinite;
+  }
+
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+
+</style>
