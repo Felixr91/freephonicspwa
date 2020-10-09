d57cb7ec78286875058824c03a38c7cc4ba1bf8d
Felix <felix@Megans-MBP-2.hitronhub.home>
2020-10-09 17:56:07 -0600

    Still importing categories

---
+++ src/components/AllCategories.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="all-categories">
+    <h1>Categories</h1>
+
+
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "AllCategories", 
+  props: {
+    categories: {
+      type: Array, 
+      required: true
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="home">
     <h1>Home Page!</h1>
+    <AllCategories/>
   </div>
 </template>
 
 <script>
 
+import AllCategories from '@/components/AllCategories.vue'
+
+import categorydata from '../data/categories.json'
+
 export default {
   name: "Home",
   components: {
-
+    AllCategories
+  }, 
+  data(){
+    return{
+      categories: categorydata
+    }
   }
 };
 </script>
