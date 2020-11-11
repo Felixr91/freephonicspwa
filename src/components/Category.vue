fa29ca1f7cc9b2e98878e98b9ca628cefb3b6258
Felix <felix@Megans-MBP-2.hitronhub.home>
2020-11-10 19:26:37 -0700

    presentation next

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
-        <h3> View all Decks in Category </h3>
+        <h3> View all Decks in {{$route.params.id}} </h3>
+
+        <v-card class="mx-auto my-12" max-width="374" v-for="deck in decks" v-bind:key="deck.id">
+        
+        <router-link v-bind:to="{name:'present', params: {id: deck.name}}">
+            <div>{{deck.name}}</div>
+        </router-link>
+        </v-card>
+
     </div>
 </template>
 
@@ -8,14 +16,24 @@
 
 import db from './firebaseInit'
 
-export default {
-    created(){
-        
-        let urlPath = window.location.pathname
-        let urlPathArray = urlPath.split('/')
-        let urlPath1 = urlPathArray[2]
 
-        console.log(urlPath1)
+export default {
+  data(){
+    return{
+      decks: []
     }
-}
+  }, 
+  created() {
+    db.collection('decks').orderBy('name').get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        console.log(doc.data())
+        const data = {
+          'id':  doc.id,
+          'name': doc.data().name
+        }
+        this.decks.push(data)
+      })
+    })
+  }
+};
 </script>
